6695b5ada779fb4907b0950ed6c4e132fbdccf91
Karan Singh <karansinghs2066@gmail.com>
2026-01-11 11:59:27 +0530

    Add LoadingSteps component

---
+++ frontend/src/LoadingSteps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingSteps() {
+  return (
+    <div style={{ marginTop: "10px", fontSize: "14px", opacity: "0.8" }}>
+      Loading...
+    </div>
+  );
+}
